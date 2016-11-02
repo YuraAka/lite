6355fd9590ab172af4700260550787dfece44f08
Юра <Yura@orion.local>
2016-11-02 09:24:16 +0300

    cont

--- match_value.py
+++ match_value.py
@@ -2,14 +2,19 @@ __author__ = 'Yura'
 
 
 class NodeType(object):
-    OWNER = 0,
-    ATTRIBUTE = 1,
-    TEXT = 2,
+    OWNER = 0,  # every node in json, tagged-node in xml
+    ATTRIBUTE = 1, # xml attribute
+    TEXT = 2,  # xml text
+    BUILTIN = 3  # int/string/double
 
 
 class Node(object):
+    @property
+    def level(self):
+        return 0 if self.parent is None else self.parent.level + 1
 
     def __init__(self):
+        self.parent = None
         self.name = None
         self.values = [] # list of Value / literals
         self.type = NodeType.REGULAR
@@ -20,6 +25,55 @@ class ExpectedNode(Node):
         self.anchor = False  # parent value will be anchored by this value
         self.absent = False  # if this value is in actual output -- it is an error
         self.order = False # preserve order of values
+        self.fixed = False # required all values matching
+
+    def diff(self, actual_node):
+        if not isinstance(actual_node, Node):
+            return NodeDiff(reason=DiffReason(DiffType.TYPES, NodeType.BUILTIN, self))
+
+        if actual_node.name != self.name:
+            if self.absent is True:
+                return None
+            return NodeDiff(reason=DiffReason(DiffType.NAMES, actual_node.name, self))
+
+        if actual_node.type != self.type:
+            return NodeDiff(reason=DiffReason(DiffType.TYPES, actual_node.type, self))
+
+        if self.order is True:
+            return self._diff_ordered_values(actual_node.values)
+        return self._diff_unordered_values(actual_node.values)
+
+    def _diff_ordered_values(self, actual_values):
+        # todo
+        pass
+
+    """
+    expected [1, 1, 2]
+    actual [1, 2]
+    error: missing value of 1
+    rate: 1/3
+    """
+    def _diff_unordered_values(self, actual_values):
+        # support fixed
+        used_actuals = set()
+
+        # todo calc rate
+        for expected_value in self.values:
+            # todo mb placeholder for none actual? preserve parents
+            min_diff = NodeDiff(reason=DiffReason(DiffType.VALUES_LESS, None, expected_value))
+            for idx, actual_value in enumerate(actual_values):
+                if idx in used_actuals:
+                    continue
+                # todo extend to builtin
+                diff = expected_value.diff(actual_value)
+                if diff is None:
+                    used_actuals.add(idx)
+                    continue
+                if min_diff > diff:
+                    min_diff = diff
+                elif min_diff == diff: # select with low level
+                    min_diff += diff
+        # todo to be continued ...
 
 
 class DiffType(object):
@@ -38,9 +92,27 @@ class DiffReason(object):
 
 
 class NodeDiff(object):
-    def __init__(self):
-        self.rate = 100 # [0, 100] int
-        self.reasons = []
+    def __init__(self, reason, rate=100):
+        assert (reason)
+        self.rate = rate  # [0, 100] int
+        self.reasons = [reason]
+
+        # from parent to child, parent mismatch has zero level
+        self.level = reason.expected.level
+
+    def __lt__(self, other):
+        return (self.level, self.rate) < (other.level, other.rate)
+
+    def __eq__(self, other):
+        return (not self < other) and (not other < self)
+
+    def __gt__(self, other):
+        return not self < other
+
+    def __iadd__(self, other):
+        if other != self:
+            raise RuntimeError('Cannot combine different level & rate diffs')
+        self.reasons += other.reasons
 
 """
 xml repr
@@ -105,35 +177,4 @@ Node
   ]
 
 """
-
-
-def make_diff(actual_node, expected_node):
-    if not isinstance(actual_node, Node):
-        raise RuntimeError('actual must be Value')
-
-    if not isinstance(expected_node, ExpectedNode):
-        raise RuntimeError('expected must be ExpectedValue')
-
-    if actual_node.name != expected_node.name:
-        diff = NodeDiff()
-        diff.reasons += [DiffReason(DiffType.NAMES, actual_node.name, expected_node.name)]
-        return diff
-
-    if actual_node.type != expected_node.type:
-        diff = NodeDiff()
-        diff.reasons += [DiffReason(DiffType.TYPES, actual_node.type, expected_node.type)]
-        return diff
-
-    min_diff = None
-    # todo ordered case
-    for expected_value in expected_node.values:
-        for actual_value in actual_node.values:
-            diff = make_diff(actual_value, expected_value)
-            if diff is None:
-                # current expected value has passed check
-                continue
-            # todo interesting
-            if min_diff is not None and min_diff.rate > diff.rate or min_diff is None:
-                min_diff = diff
-
 
