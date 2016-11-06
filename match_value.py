ac4e2b09618eb62331215b973c846869276fa0f5
Юра <Yura@orion.local>
2016-11-06 18:35:58 +0300

    save

--- match_value.py
+++ match_value.py
@@ -1,108 +1,24 @@
+import collections
+
 __author__ = 'Yura'
 
 
 class NodeType(object):
-    OWNER = 0,  # every node in json, tagged-node in xml
+    REGULAR = 0,  # every node in json, tagged-node in xml
     ATTRIBUTE = 1, # xml attribute
-    TEXT = 2,  # xml text
-    BUILTIN = 3  # int/string/double
 
 
-class Node(object):
-    @property
-    def level(self):
-        return 0 if self.parent is None else self.parent.level + 1
-
-    def __init__(self):
-        self.parent = None
-        self.name = None
-        self.values = [] # list of Value / literals
-        self.type = NodeType.REGULAR
-
-
-class ExpectedNode(Node):
-    def __init__(self):
-        self.anchor = False  # parent value will be anchored by this value
-        self.absent = False  # if this value is in actual output -- it is an error
-        self.order = False # preserve order of values
-        self.fixed = False # required all values matching
-
-    def diff(self, actual_node):
-        if not isinstance(actual_node, Node):
-            return NodeDiff(reason=DiffReason(DiffType.MISS_TYPES, actual_node, self))
-
-        if actual_node.name != self.name:
-            if self.absent is True:
-                return None
-            return NodeDiff(reason=DiffReason(DiffType.NAMES, actual_node, self))
-
-        if actual_node.type != self.type:
-            return NodeDiff(reason=DiffReason(DiffType.MISS_TYPES, actual_node, self))
-
-        if self.order is True:
-            return self._diff_ordered_values(actual_node.values)
-        return self._diff_unordered_values(actual_node.values)
-
-    def _diff_ordered_values(self, actual_values):
-        # todo
-        pass
-
-    """
-    expected [1, 1, 2]
-    actual [1, 2]
-    error: missing value of 1
-    rate: 1/3
-    """
-    def _diff_unordered_values(self, actual_values):
-        # todo support fixed
-        used_actuals = set()
-        value_list_diff = NodeDiff(reason=DiffReason(DiffType.VALUES_DIFF, actual_values, self.values))
-        for expected_value in self.values:
-            value_diff = NodeDiff(reason=DiffReason(DiffType.VALUE_NOT_FOUND, actual_values, expected_value))
-            for idx, actual_value in enumerate(actual_values):
-                if idx in used_actuals:
-                    continue
-                # todo extend to builtin
-                candidate_diff = expected_value.diff(actual_value)
-                if candidate_diff is None:
-                    used_actuals.add(idx)
-                    value_diff = None
-                    value_list_diff.hits += 1
-                    break
-                value_diff.children.append(candidate_diff)
-            else:
-                value_list_diff.misses += 1
-                value_list_diff.children.append(value_diff)
-
-        return None if value_list_diff.misses == 0 else value_list_diff
-
-    def _diff_side_by_side(self, expected, actual):
-        if isinstance(expected, ExpectedNode):
-            return expected.diff(actual)
-        if type(expected) != type(actual):
-            return NodeDiff(reason=DiffReason(DiffType.MISS_TYPES, actual, expected))
-        if isinstance(expected, list):
-            return self._diff_builtin_lists(expected, actual)
-        elif type(expected) in [int, str, unicode]:
-            # TODO
-            pass
-        else:
-            return NodeDiff(reason=DiffReason(DiffType.UNK_TYPE, actual, expected))
-
-    def _diff_builtin_lists(self, expected, actual):
-        # TODO
-        pass
-
-
-class DiffType(object):
-    MISS_TYPES = 0
-    NAMES = 1
+class DiffKind(object):
+    TYPES_NOT_EQUAL = 0
+    NAME_NOT_EQUAL = 1
     VALUES_MORE = 2
     VALUES_LESS = 3
-    VALUES_DIFF = 4
+    CHILDREN_DIFF = 4
     VALUE_NOT_EQUAL = 5
-    VALUE_NOT_FOUND = 6
+    CHILD_NOT_FOUND = 6
     UNK_TYPE = 7
+    VALUE_UNEXP_EQUAL = 8
+    VALUE_FIX_FAILURE = 9
 
 
 # todo make classes for each type
@@ -114,11 +30,120 @@ class DiffReason(object):
 
 
 class NodeDiff(object):
-    def __init__(self, reason, rate=100):
+    def __init__(self, kind, actual, expected):
         self.hits = 0
         self.misses = 0
-        self.reasons = [reason]
         self.children = []
+        self.actual = actual
+        self.expected = expected
+        self.kind = kind
+
+
+class Node(object):
+    def __init__(self):
+        self.parent = None
+        self.name = None
+        self.value = None   # built-in value or matcher
+        self.children = []  # list of Values or ExpectedValues
+        self.type = NodeType.REGULAR
+
+
+class ExpectedNode(Node):
+    def __init__(self):
+        self.anchor = False  # if this value is not found, do not process siblings
+        self.absent = False  # if this value is in actual output -- it is an error
+        self.order = False # preserve order of children
+        self.fixed = False # required children match, otherwise -- part is sufficient
+
+    def diff(self, actual):
+        if not isinstance(actual, Node):
+            return NodeDiff(DiffKind.TYPES_NOT_EQUAL, actual, self)
+
+        if actual.name != self.name:
+            if self.absent is True:
+                return None
+            return NodeDiff(DiffKind.NAME_NOT_EQUAL, actual, self)
+
+        if actual.type != self.type:
+            return NodeDiff(DiffKind.TYPES_NOT_EQUAL, actual, self)
+
+        if actual.value != self.value:
+            return NodeDiff(DiffKind.VALUE_NOT_EQUAL, actual, self)
+
+        if self.order is True:
+            return self._diff_children_with_order(actual)
+        return self._diff_children_no_order(actual)
+
+    def _diff_children_with_order(self, actual_values):
+        # todo
+        pass
+
+    """
+    expected [1, 1, 2]
+    actual [1, 2]
+    error: missing value of 1
+    rate: 1/3
+    """
+    def _diff_children_no_order(self, actual_parent):
+        # todo support fixed
+        # todo support anchor
+        matched_actuals = set()
+        matched_expected = set()
+
+        # expected_idx => (actual_idx => diff)
+        hypothesis = collections.defaultdict(dict)
+        children_diff = NodeDiff(DiffKind.CHILDREN_DIFF, actual_parent, self)
+
+        # matrix expected vs actual
+        # ordered and unordered ways of traverse
+
+        # collect all matched values
+        for expected_idx, expected in enumerate(self.children):
+            for actual_idx, actual in enumerate(actual_parent.children):
+                if actual_idx in matched_actuals:
+                    continue
+                candidate_diff = self._diff_side_by_side(actual, expected)
+                if candidate_diff is None:
+                    matched_actuals.add(actual_idx)
+                    matched_expected.add(expected_idx)
+                    children_diff.hits += 1
+                    break
+                hypothesis[expected_idx][actual_idx] = candidate_diff
+            else:
+                children_diff.misses += 1
+
+        if children_diff.misses == 0:
+            return None
+
+        # collect hypothesis
+        for expected_idx, expected in enumerate(self.children):
+            if expected_idx in matched_expected:
+                continue
+            child_diff = NodeDiff(DiffKind.CHILD_NOT_FOUND, actual_parent, expected)
+            for actual_idx, actual in enumerate(actual_parent.children):
+                if actual_idx in matched_actuals:
+                    continue
+                child_diff.children.append(hypothesis[expected_idx][actual_idx])
+            children_diff.children.append(child_diff)
+        return children_diff
+
+    def _diff_side_by_side(self, actual, expected):
+        if isinstance(expected, ExpectedNode):
+            return expected.diff(actual)
+        if type(expected) != type(actual):
+            return NodeDiff(reason=DiffReason(DiffKind.TYPES_NOT_EQUAL, actual, expected))
+        if isinstance(expected, list):
+            return self._diff_builtin_lists(expected, actual)
+        if type(expected) in [int, str, unicode]:
+            return self._diff_builtin_atoms(expected, actual)
+        return NodeDiff(reason=DiffReason(DiffKind.UNK_TYPE, actual, expected))
+
+    def _diff_builtin_atoms(self, expected, actual):
+        # TODO support matcher
+        if expected != actual:
+            return NodeDiff(reason=DiffReason(DiffKind.VALUE_NOT_EQUAL, actual, expected))
+        return None
+
 
 """
 xml repr
